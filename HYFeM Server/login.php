fa62de9a26e64a213f5edb594d33f70f95e4d3eb
ryj0902 <ryj0902@naver.com>
2013-09-27 01:16:23 +0900

    Delete README, login
    Delete those files with protection.

--- HYFeM Server/login.php
+++
@@ -1,8 +0,0 @@
-<? header("content-type:text/html; charset=utf-8");?>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<?php
-	$db_host = "localhost"; 
-	$db_user = "flysheep"; 
-	$db_passwd = "hyfem123";
-	$db_name = "flysheep";
-?>
